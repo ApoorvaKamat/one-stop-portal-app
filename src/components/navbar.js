e23906a5d8d7e30401e659aa5bea4f72904041a2
Apoorva Kamat <apoorvaKamat885@outlook.com>
2022-10-30 12:38:12 -0500

    update Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,36 +2,44 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 export function NavBar() {
   return (
-    <Navbar bg="dark" expand="sm">
-      <Container>
-      <Navbar.Brand href="#home" className="text-white">One Stop Graphica Portal</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar  collapseOnSelect bg="dark" expand="sm" variant="dark">
+      <Navbar.Brand href="#home" className="px-3">One Stop Graphica Portal</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="esponsive-navbar-nav">
           <Nav fill className="w-100">
             <Nav.Item>
-                <Nav.Link href="#support" className="text-white">Support</Nav.Link>
+                <Nav.Link href="#support">Support</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="#hrConnect" className="text-white">HR-Connect</Nav.Link>
+                <Nav.Link href="#hrConnect">HR-Connect</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="#projectHelp" className="text-white">Project Help</Nav.Link>
+                <Nav.Link href="#projectHelp">Project Help</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="#knowYourCompnay" className="text-white">Know Your Company</Nav.Link>
+                <Nav.Link href="#knowYourCompnay">Know Your Company</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="#network" className="text-white">Network</Nav.Link>
+                <Nav.Link href="#network">Network</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#network" className="text-white">Logout</Nav.Link>
-            </Nav.Item>
+            <NavDropdown className="text-white" title="Hi, Jhon Doe" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Settings
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
   );
 }
