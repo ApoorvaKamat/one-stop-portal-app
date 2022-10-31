3fa32f4e1e02bcbdd3f31f588117a4e8afcdf2d0
sajalsuhane <ssuhane31@gmail.com>
2022-10-30 21:46:23 -0500

    up

--- src/components/task-news-learn.js
+++ src/components/task-news-learn.js
@@ -14,13 +14,13 @@ export function TaskNewsLearn() {
                     <Row>
                         <Col className="bg-subhead"><h4 className="pt-2 text-white">TODAY'S TASKS</h4></Col>
                     </Row>
-                    <Row className="h-25 overflow-auto">
+                    <Row className="h-35 overflow-auto">
                         <TaskTable />
                     </Row>
                     <Row>
                         <Col className="bg-subhead"><h4 className="pt-2 text-white">CALENDAR</h4></Col>
                     </Row>
-                    <Row className="h-50 overflow-auto">
+                    <Row className="h-50">
                         <Calendar />{/*Add calender here*/}
                     </Row>
                 </Col>
