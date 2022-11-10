f4f7c5797e6542003ea3e24d38348feb1cf996dc
sajalsuhane <ssuhane31@gmail.com>
2022-11-09 19:48:51 -0600

    JSON for faq and contacts

---
+++ src/assets/faq.js
@@ -0,0 +1,37 @@
+export const faq =[
+    {
+        id:1,
+        title: 'How to push code for faster PR approval?',
+        description: 'Pull requests are at the core of using GitHub, regardless of whether we are talking about open-source or private projects.',
+        readmore: 'In this post, I will talk about a super-specific—yet very valuable—aspect of the pull request process: the different ways in which you can approve a pull request on GitHub.',
+        imageSrc: '/pullrequest.jpeg',
+    },
+    {
+        id:2,
+        title: 'How to work with VR/AR? Beginners guide.',
+        description: 'VR, AR, and MR workers are transforming the world across all industries. They get to do fulfilling work in some of the best companies in the world and earn quite a lot of money.',
+        readmore: 'With the ever growing hardware adoption — VR headsets like Oculus Rift, Oculus a.k.a Meta Quest 2 and HTC Vives are having record sales in 2020, the real world is in need of virtual reality developers, or some other XR-related professionals.',
+        imageSrc: '/arvr.jpeg',
+    },
+    {
+        id:3,
+        title: 'How to use internal HCI tool?',
+        description: 'Some ground-breaking Creation and Graphic Communication designers started showing interest in the field of HCI from the late 80s.',
+        readmore: 'Others crossed the threshold by designing program for CD ROM titles. Some of them entered the field by designing for the web and by providing computer trainings.',
+        imageSrc: '/hci.png',
+    },
+    {
+        id:4,
+        title: 'How to make Windows Application using internal tools?',
+        description: 'The Windows API (also known as the Win32 API, Windows Desktop API, and Windows Classic API) is a C-language-based framework for creating Windows applications. ',
+        readmore: 'Follow these steps to create your first Windows desktop project. As you go, you will enter the code for a working Windows desktop application.',
+        imageSrc: '/windowsapplication.png',
+    },
+    {
+        id:5,
+        title: 'How to make iOS Application using internal tools?',
+        description: 'iOS is Apple’s mobile OS that runs on an iPhone, iPad, iPod Touch hardware.',
+        readmore: 'Apple provides tools and resources for creating iOS apps and accessories for these devices. As an iOS developer, you can program in native languages such as Swift or Objective-C or build cross-platform native applications using React Native (JavaScript) or Xamarin (C# & F#).',
+        imageSrc: '/ios.png',
+    },
+]
