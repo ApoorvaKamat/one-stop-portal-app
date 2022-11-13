d52437ca9aba855b926be4f08e641a960cf12b2b
Apoorva Kamat <apoorvaKamat885@outlook.com>
2022-11-13 16:46:35 -0600

    Message Dialogue box fix

--- src/components/MessageInputSvg.js
+++ src/components/MessageInputSvg.js
@@ -7,6 +7,9 @@ import {
     MessageList,
     Message,
     MessageInput,
+    MessageGroup,
+    MessageListContent,
+    MessageSeparator,
 } from "@chatscope/chat-ui-kit-react";
 import * as Icon from 'react-bootstrap-icons';
 import {nanoid} from "nanoid";
@@ -28,14 +31,32 @@ const serviceFactory = (storage, updateState) => {
 const chatStorage = new BasicStorage({groupIdGenerator, messageIdGenerator});
 
 export function MessageInputSvg(props) {
+    const msgArray = ['Hi','Oh I see, can we discuss this over a call', 'Thankyou','Bye']
     const [newmsg, setnewmsg] = useState([]);
     const [show, setShow] = useState(false);
+    const [isHover, setIsHover]= useState('');
+    const [returnMsg, setreturnMsg]= useState('');
+    const [indexCounter, setIndexCounter]= useState(0);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handlemouseenter = () => setIsHover('Green');
+    const handleMouseLeave = () => setIsHover('Blue');
 
-    const onmsgsend = () => setnewmsg([...newmsg, document.getElementById("id").value]); 
+    const onmsgsend = () => {
+        let value = document.getElementById("id").value;
+        let index = indexCounter;
+        if(value !== ""){
+            setreturnMsg(msgArray[index]);
+            setnewmsg([...newmsg, value,msgArray[index]]);
+            index += 1;
+            setIndexCounter(index);}
 
+            else{
+                setIsHover('Red');
+            }
+        }
+        
 
     return (
         <>
@@ -47,9 +68,9 @@ export function MessageInputSvg(props) {
                 <Modal.Header closeButton>
                     <Modal.Title>{props.name}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ position: "relative", height: "500px" }}>
+                <Modal.Body>
                 <MainContainer>
-                        <ChatContainer>
+                        <ChatContainer style={{minHeight: "25rem",}}>
                             <MessageList>
                                 <Message
                                     model={{
@@ -61,29 +82,40 @@ export function MessageInputSvg(props) {
                                     }}
                                 /> 
                                 {
-                                     newmsg.map(i => {
-                                        return <Message
+                                newmsg.map((i,index)=>(
+                                        <Message key={index}
                                         model={{
                                             message: i,
                                             sentTime: "just now",
                                             sender: "Joe",
-                                            direction: "outgoing"
+                                            direction: `${index%2 ==0 ? "outgoing":"incoming"}`
     
                                         }}
-                                    /> 
-                                    })
-
-                                }
+                                    />  
+                                ))
+                            }
                             </MessageList>
                             
                         </ChatContainer>
                     </MainContainer>
-                    <div style={{ width: "100%" }}>
+                    <div className='d-flex flex-row pt-3'>
                     <input
                                 id='id'
-                                placeholder="Type message here"
+                                type="text" 
+                                className={`form-control w-75 text-start`} 
+                                placeholder="Type you message here" 
+                                aria-label="message"
+                                
                             />
-                           <button onClick={onmsgsend}>Send</button>
+                    <Icon.Send 
+                        color={isHover} 
+                        size={30} 
+                        onClick={onmsgsend} 
+                        onMouseEnter={handlemouseenter} 
+                        onMouseLeave={handleMouseLeave} 
+                        className="m-2">
+                            Send
+                    </Icon.Send >
                     </div>
                     
 
